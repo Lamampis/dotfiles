a921f9fc2b3f316658496ea35fa735d1d4362062
Lamampis <82538787+Lamampis@users.noreply.github.com>
2024-12-30 20:44:04 +0200

    updating

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,10 +5,11 @@ end
 set fish_greeting
 
 export EDITOR="/usr/bin/vim"
-alias ihatemylife="doas emerge -avuDt --changed-use --with-bdeps=y @world" 
+alias ihatemylife="doas emerge --getbinpkg -avuDt --changed-use --with-bdeps=y @world" 
 alias sudo="doas"
 alias reboot="doas reboot"
 alias shutdown="doas openrc-shutdown --poweroff now"
+#alias wf-recorder=(wf-recorder --audio -f /home/lampis/Videos/Recording-"$(date +"%T-%d-%m-%y")".mkv -g "$(slurp)")
 
 # Created by `pipx` on 2024-06-20 17:59:21
 set PATH $PATH /home/lampis/.local/bin
